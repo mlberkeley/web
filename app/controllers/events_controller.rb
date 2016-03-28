2a29360f16fc78715c959c31a702f4fdaeae9558
Irene Lee <sohyeonlee@berkeley.edu>
2016-03-28 12:23:35 -0700

    pushing to heroku for now

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,7 +9,7 @@ class EventsController < ApplicationController
 
 private
   def get_events
-    graph = Koala::Facebook::API.new()
+    graph = Koala::Facebook::API.new("CAAYGzuK8lbwBAMWhUbAyROZASalHMQLTyeaZBeefRDi8VDZBSiam4h8ZBDlo5VnK75n7JXwJHwUCrmaTeOtcgn420oZCOVMpDfCaucOBJIAbVfR1iTKuJ29oOEhOFj7qLnPdk0XNf6zhePzlzbhLkvNudkbzaqHXQR0adj7ZCxMrcARWlfLDD41kCWsDHcOZC4iL3uDQhSccgZDZD")
     events = graph.get_object("v2.5/1701763616733787/events")
   end
 
